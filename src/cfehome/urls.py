fb754c50b92c7bf2c553f1abc1a38491aa5043f2
Arun Reddy <arunreddy@Aruns-MacBook-Air.local>
2025-05-05 20:40:44 +0530

    initial commit

---
+++ src/cfehome/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for cfehome project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from .views import home_view, about_view, pw_protected_view, user_only_view
+from auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('login/',auth_views.login_view),
+    path('register/',auth_views.register_view),
+    path('', home_view, name='home'),
+    path('hello-world/', home_view),
+    path('hello-world.html', home_view),
+    path('about/', about_view),
+    path('protected/', pw_protected_view),
+    path('protected/user-only', user_only_view),
+]
